cbf91f29f6a49f1467aa7b7284628823fdf435f2
Aadhi-a <akashaadhi5877@gmail.com>
2025-11-19 17:38:03 +0530

    folders config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["module:@react-native/babel-preset"],
+
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@features": "./src/app/features",
+            "@navigation": "./src/app/navigation",
+            "@components": "./src/app/components",
+            "@unistyles": "./src/app/unistyles",
+            "@utils": "./src/app/utils",
+            "@screens": "./src/app/screens",
+          },
+        },
+      ],
+    ],
+  };
 };
